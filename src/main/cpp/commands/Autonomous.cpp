57b076a72b26039ee5d1664d96d6180ef6abe112
Rob Herbst <rherbst17@gmail.com>
2020-01-30 19:15:05 -0600

    Remove RampUpVelocityTest
    The class is unused and not working.

--- src/main/cpp/commands/Autonomous.cpp
+++ src/main/cpp/commands/Autonomous.cpp
@@ -1,14 +1,12 @@
 #include "commands/Autonomous.h"
 #include "commands/Move.h"
 #include "commands/VelocityTest.h"
-#include "commands/RampUpVelocityTest.h"
 #include "commands/DriveStraightDistance.h"
 
 Autonomous::Autonomous(int selection) : CommandBase("Autonomous") {
   //std::string gameData;
   //gameData = frc::DriverStation::GetInstance().GetGameSpecificMessage();
   //AddSequential(new VelocityTest());
-  //AddSequential(new RampUpVelocityTest());
   //AddSequential(new DriveStraightDistance(36));
   //"Wes is Stupid and dumb" -Wes
 
