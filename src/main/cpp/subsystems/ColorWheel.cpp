57f9300d97a2dc62a6b0789f4152b364d0a50717
Rob Herbst <rherbst17@gmail.com>
2020-02-01 23:28:54 -0600

    ColorWheel: Add color match APIs
    Add APIs to get the RGB directly from the color sensor. Also,
    add initial support for color matching.  The Blue, Green, Red,
    and Yellow values will likely need to be tweaked.

--- src/main/cpp/subsystems/ColorWheel.cpp
+++ src/main/cpp/subsystems/ColorWheel.cpp
@@ -1,13 +1,54 @@
 #include "subsystems/ColorWheel.h"
 
+// TODO: Set these colors based on our sensor
+static constexpr frc::Color sBlueTarget = frc::Color(0.143, 0.427, 0.429);
+static constexpr frc::Color sGreenTarget = frc::Color(0.197, 0.561, 0.240);
+static constexpr frc::Color sRedTarget = frc::Color(0.561, 0.232, 0.114);
+static constexpr frc::Color sYellowTarget = frc::Color(0.361, 0.524, 0.113);
+
 ColorWheel::ColorWheel() 
     : Subsystem("ColorWheel")
     , mColorSensor( frc::I2C::Port::kOnboard )
 {
-
+    mColorMatcher.AddColorMatch(sBlueTarget);
+    mColorMatcher.AddColorMatch(sGreenTarget);
+    mColorMatcher.AddColorMatch(sRedTarget);
+    mColorMatcher.AddColorMatch(sYellowTarget);
 }
 
 ColorWheel::~ColorWheel()
 {
 
+}
+
+frc::Color ColorWheel::getColor()
+{
+    return mColorSensor.GetColor();
+}
+
+ColorWheel::MatchedColor ColorWheel::getMatchedColor()
+{
+    double confidence = 0.0;
+    frc::Color detectedColor = getColor();
+    frc::Color matchedColor = mColorMatcher.MatchClosestColor( detectedColor, confidence );
+
+    MatchedColor matchValue = NO_MATCH;
+    if( sBlueTarget == matchedColor )
+    {
+        matchValue = BLUE;
+    }
+    else if( sGreenTarget == matchedColor )
+    {
+        matchValue = GREEN;
+    }
+    else if( sRedTarget == matchedColor )
+    {
+        matchValue = RED;
+    }
+    else if( sYellowTarget == matchedColor )
+    {
+        matchValue = YELLOW;
+    }
+
+    return matchValue;
 }
--- src/main/include/subsystems/ColorWheel.h
+++ src/main/include/subsystems/ColorWheel.h
@@ -2,14 +2,29 @@
 
 #include <frc/commands/Subsystem.h>
 #include "rev/ColorSensorV3.h"
+#include "rev/ColorMatch.h"
 
 class ColorWheel : public frc::Subsystem {
     public:
+        enum MatchedColor
+        {
+            BLUE,
+            GREEN,
+            RED,
+            YELLOW,
+            NO_MATCH
+        };
+
         ColorWheel();
 
         virtual ~ColorWheel();
 
+        frc::Color getColor();
+
+        MatchedColor getMatchedColor();
+
     private:
         rev::ColorSensorV3 mColorSensor;
+        rev::ColorMatch mColorMatcher;
 
 };
